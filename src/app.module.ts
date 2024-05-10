0bb258e9955693cc4a2f90a7466bc5190aed376d
Sebastian Perez <spezuluaga@gmail.com>
2024-05-10 17:33:45 -0500

    ft/connecting-to-docker-postgres-database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [ConfigModule.forRoot()]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_NAME,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      // synchronize (prod -> false) | (dev -> true)
+      synchronize: false
+    })
+  ]
 })
 export class AppModule {}
