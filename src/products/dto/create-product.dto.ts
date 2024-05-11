125da3c912436332f57bb66537769835f692093d
Sebastian Perez <spezuluaga@gmail.com>
2024-05-11 15:20:11 -0500

    ft/setting-up-products-DTOs

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString
+} from 'class-validator';
+import { GENDERS, SIZES } from './constants/product.constants';
+
+// How do I want the body data of the petition to get to me?
+export class CreateProductDto {
+  @IsString()
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  stock: number;
+
+  @IsString({ each: true })
+  @IsIn(SIZES, { each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(GENDERS)
+  gender: string;
+}
