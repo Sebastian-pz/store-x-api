6bb5ad84dfd186faf8d6d15fbfcc382ba1cb5bcb
Sebastian Perez <spezuluaga@gmail.com>
2024-05-11 17:54:00 -0500

    ft/basic-crud-products

---
+++ src/products/products.service.ts
@@ -0,0 +1,79 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import ErrorHandler from 'src/common/handler/error.handler';
+
+@Injectable()
+export class ProductsService {
+  // Patron repositorio
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    private readonly errorHandler: ErrorHandler
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      // Creating instance of product
+      const product = this.productRepository.create(createProductDto);
+
+      // Saving product in the database
+      await this.productRepository.save(product);
+
+      return product;
+    } catch (error) {
+      this.errorHandler.handle('Product/Service - Create', error);
+    }
+  }
+
+  async findAll() {
+    try {
+      const products = await this.productRepository.find();
+      return products;
+    } catch (error) {
+      this.errorHandler.handle('Product/Service - findAll', error);
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      const productToFind = await this.productRepository.findOneBy({ id });
+      if (!productToFind)
+        throw new NotFoundException(`Product ${id} not found`);
+
+      return productToFind;
+    } catch (error) {
+      this.errorHandler.handle('Product/Service - findOne', error);
+    }
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const update = await this.productRepository.update(
+        { id },
+        updateProductDto
+      );
+
+      if (!update.affected)
+        throw new NotFoundException(`Product ${id} not found and updated`);
+
+      return { message: 'updated successfully', status: 200 };
+    } catch (error) {
+      this.errorHandler.handle('Products/Service - update', error);
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      const productToDeleted = await this.productRepository.delete(id);
+      if (productToDeleted.affected)
+        return { message: 'removed successfully', status: 200 };
+      throw new NotFoundException(`Product ${id} not found`);
+    } catch (error) {
+      this.errorHandler.handle('Product/Service - Remove', error);
+    }
+  }
+}
