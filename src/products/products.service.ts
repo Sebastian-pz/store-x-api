d8268bc1b7e50e4cd3f7ecb3b296abeb8201f247
Sebastian Perez <spezuluaga@gmail.com>
2024-05-11 21:07:13 -0500

    refactor/get-product-can-now-use-slug-as-search-query

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import ErrorHandler from 'src/common/handler/error.handler';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -43,13 +44,17 @@ export class ProductsService {
     }
   }
 
-  async findOne(id: string) {
+  async findOne(term: string) {
     try {
-      const productToFind = await this.productRepository.findOneBy({ id });
-      if (!productToFind)
-        throw new NotFoundException(`Product ${id} not found`);
+      let product: Product;
 
-      return productToFind;
+      if (isUUID(term))
+        product = await this.productRepository.findOneBy({ id: term });
+      else product = await this.productRepository.findOneBy({ slug: term });
+
+      if (!product) throw new NotFoundException(`Product ${term} not found`);
+
+      return product;
     } catch (error) {
       this.errorHandler.handle('Product/Service - findOne', error);
     }
