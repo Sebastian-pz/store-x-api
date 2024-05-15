8b2b6bda564dcf9d505301e0c91150ce49953e80
Sebastian Perez <spezuluaga@gmail.com>
2024-05-15 18:05:38 -0500

    ft/setting-custom-data-to-decorator

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -6,15 +6,26 @@ import {
 
 export const GetUser = createParamDecorator(
   (data, context: ExecutionContext) => {
-    console.log(data);
-
     const request = context.switchToHttp().getRequest();
-
     const { user } = request;
 
     if (!user)
       throw new InternalServerErrorException('[Server] user not found');
 
-    return user;
+    if (typeof data !== 'string')
+      throw new InternalServerErrorException(
+        `[Server] Invalid requirement, expected string and received ${typeof data}}`
+      );
+
+    switch (data) {
+      case 'roles':
+        return { roles: user.roles };
+
+      case 'isActive':
+        return { isActive: user.isActive };
+
+      default:
+        return user;
+    }
   }
 );
