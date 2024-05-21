b014f0b2fc5481322ecf497ee958c309d7cec31f
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:42:21 -0500

    ft/doc-login-user-dto

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({ example: 'client@emial.com' })
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '*******' })
   @IsString()
   password: string;
 }
