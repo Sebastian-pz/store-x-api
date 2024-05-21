99f89acaf6ee6144761ba848703b2480c635b623
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:41:06 -0500

    ft/doc-create-product-dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -12,36 +12,45 @@ import { GENDERS, SIZES } from './constants/product.constants';
 import { ProductImage } from '../entities';
 import { Type } from 'class-transformer';
 import { ProductImageDto } from './productImage.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 // How do I want the body data of the petition to get to me?
 export class CreateProductDto {
+  @ApiProperty()
   @IsString()
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   stock: number;
 
+  @ApiProperty({ example: [SIZES[0], SIZES[1]], enum: SIZES })
   @IsString({ each: true })
   @IsIn(SIZES, { each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({ enum: GENDERS })
   @IsIn(GENDERS)
   gender: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
