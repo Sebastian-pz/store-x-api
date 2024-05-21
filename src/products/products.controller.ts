874a4068e243e9fea680a4ea466fd281e75f7ce6
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:31:28 -0500

    ft/basic-doc-product-controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,7 +9,7 @@ import {
   ParseUUIDPipe,
   Query
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -17,6 +17,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { User } from 'src/auth/entities/user.entity';
+import { Product } from './entities';
 
 @ApiTags('Products')
 @Controller('products')
@@ -25,6 +26,19 @@ export class ProductsController {
 
   @Post()
   @Auth()
+  @ApiResponse({
+    status: 201,
+    description: 'Product created',
+    type: Product
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request'
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Token related'
+  })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(createProductDto, user);
   }
