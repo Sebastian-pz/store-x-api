234300b2e8a6824198697fdc19834fb815e0e752
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:36:25 -0500

    ft/doc-pagination-dto

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({ default: 10 })
   @IsOptional()
   @IsNumber()
   @IsPositive()
   @Type(() => Number) // Parse datatypes
   limit?: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsNumber()
   @IsPositive()
