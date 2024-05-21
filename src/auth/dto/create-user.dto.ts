d38558c3e0311529bd6296229b52925d63016770
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:56:00 -0500

    ft/doc-create-user-dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   fullName: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
