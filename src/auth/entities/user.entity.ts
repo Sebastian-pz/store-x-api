3e2d5c7cc38d7b840892cca6c80c45cfca008ddc
Sebastian Perez <spezuluaga@gmail.com>
2024-05-20 20:30:17 -0500

    ft/doc-user-entity

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -8,28 +8,37 @@ import {
 } from 'typeorm';
 import USER_ROLES from '../constants/roles';
 import { Product } from 'src/products/entities';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({ uniqueItems: true })
   @Column('text', {
     unique: true
   })
   email: string;
 
+  @ApiProperty()
   @Column('text')
   password: string;
 
+  @ApiProperty()
   @Column('text')
   fullName: string;
 
+  @ApiProperty({ default: true })
   @Column('bool', {
     default: true
   })
   isActive: boolean;
 
+  @ApiProperty({
+    enum: USER_ROLES,
+    example: [USER_ROLES.client]
+  })
   @Column('text', {
     array: true,
     default: [USER_ROLES.client]
